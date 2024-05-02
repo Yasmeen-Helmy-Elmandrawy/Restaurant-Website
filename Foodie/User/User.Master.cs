fc144e98e96137358cf24e72145f6af18b565eb0
Yasmeen Helmy <76138173+Yasmeen-Helmy-Elmandrawy@users.noreply.github.com>
2024-05-02 23:33:46 +0300

    Create Project

---
+++ Foodie/User/User.Master.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Foodie.User
+{
+    public partial class User : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Request.Url.AbsoluteUri.ToString().Contains("Default.aspx"))
+            {
+                form1.Attributes.Add("class", "sub_page");
+            }
+            else
+            {
+                form1.Attributes.Remove("class");
+                //load the control
+                Control sliderUserControl = (Control)Page.LoadControl("SliderUserControl.ascx");
+                //Add the control to the panel
+                Panel1.Controls.Add(sliderUserControl);
+                //pnlSliderUC.Controls.Add(sliderUserControl);
+            }
+        }
+    }
+}
